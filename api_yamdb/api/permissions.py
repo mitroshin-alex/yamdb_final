cde592d72d03f356337859c824917f506a5e07b9
Aleksey Mitroshin <mitroshin.alex@gmail.com>
2022-09-18 18:47:16 +0300

    FIX permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -47,6 +47,7 @@ class IsModeratorAutherAdminOrReadOnly(BasePermission):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return (obj.author == request.user
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
                 or request.user.is_admin()
                 or request.user.is_moderator())
